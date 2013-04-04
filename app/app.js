d3962ce0e48495205e0da3e9fb773b7056b42532
Myk Melez <myk@mozilla.org>
2013-04-04 10:42:42 -0700

    scream screen

--- app/app.js
+++ app/app.js
@@ -3,3 +3,7 @@ function geolocate() {
     alert("Your position is " + position.coords.latitude + " x " + position.coords.longitude + ".");
   });
 }
+
+function screamScreen() {
+  alert("screen: " + window.screen.width + "x" + window.screen.height);
+}
