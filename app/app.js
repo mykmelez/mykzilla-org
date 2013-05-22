68e584c3472cc0db54fae4f55ff47d63a69fc8d8
Myk Melez <myk@mozilla.org>
2013-05-22 16:02:26 -0700

    watch position instead of merely getting current one

--- app/app.js
+++ app/app.js
@@ -1,6 +1,9 @@
 function geolocate() {
-  navigator.geolocation.getCurrentPosition(function(position) {
-    alert("Your position is " + position.coords.latitude + " x " + position.coords.longitude + ".");
+  navigator.geolocation.watchPosition(function(position) {
+    var text = document.createTextNode(position.coords.latitude + " x " + position.coords.longitude);
+    var div = document.createElement("div");
+    div.appendChild(text);
+    document.body.appendChild(div);
   });
 }
 
