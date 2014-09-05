a3fb4f5ced9d49910478cd74026f0b5bcb9d1abb
Myk Melez <myk@mozilla.org>
2014-09-05 11:42:22 -0700

    add useDeviceStorage function

--- app/app.js
+++ app/app.js
@@ -75,3 +75,20 @@ function install(manifestName) {
     alert(message);
   }
 }
+
+function useDeviceStorage() {
+  var sdcard = navigator.getDeviceStorage("sdcard");
+  var file   = new Blob(["This is a text file."], {type: "text/plain"});
+
+  var request = sdcard.addNamed(file, "my-file.txt");
+
+  request.onsuccess = function () {
+    var name = this.result;
+    console.log('File "' + name + '" successfully wrote on the sdcard storage area');
+  }
+
+  // An error typically occur if a file with the same name already exist
+  request.onerror = function () {
+    console.warn('Unable to write the file: ' + this.error);
+  }
+}
